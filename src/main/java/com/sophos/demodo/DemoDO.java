b6bb0d2bfb7a86e6b2bdff218bb9f0295a08834d
SBMEDLAPFABRR1\RICARDO.RAMIREZ <rickyrb@gmail.com>
2018-11-23 14:13:01 -0500

    Se mejora el código y la visualización

--- src/main/java/com/sophos/demodo/DemoDO.java
+++ src/main/java/com/sophos/demodo/DemoDO.java
@@ -4,21 +4,56 @@ import java.util.Scanner;
 
 public class DemoDO {
 
+	/**
+	 * Método principal
+	 * @param args argumentos de línea de comandos
+	 */
 	public static void main(String[] args) {
+		// Mensaje de bienvenida
 		System.out.println("Bienvenido al modulo de aritmetica basica");
+		System.out.println("=========================================");
 		System.out.println();
-		System.out.println("Ingrese el primer argumento: ");
-		final Scanner scanner = new Scanner(System.in);
-		final int arg1 = scanner.nextInt();
-		System.out.println("Ingrese el segundo argumento: ");
-		final int arg2 = scanner.nextInt();
 
+		// Recibe el primer argumento
+		System.out.println("Ingrese el primer argumento: ");
+		final int arg1 = leerEnteroDeConsola();
+
+		// Recibe el segundo argumento
+		System.out.println("Ingrese el segundo argumento: ");
+		final int arg2 = leerEnteroDeConsola();
+
+		// Presenta los resultados
 		System.out.println();
 		System.out.println("Resultados");
-		System.out.println("Suma de los argumentos:            " + sumar(arg1, arg2));
-		System.out.println("Resta de los argumentos:           " + restar(arg1, arg2));
-		System.out.println("Producto de los argumentos:        " + multiplicar(arg1, arg2));
-		System.out.println("Cociente entero de los argumentos: " + dividir(arg1, arg2));
+		System.out.println("----------------------------------------------------------");
+		System.out.println("Suma de los argumentos:            (" + arg1 + " + " + arg2 + ") = " + sumar(arg1, arg2));
+		System.out.println("Resta de los argumentos:           (" + arg1 + " - " + arg2 + ") = " + restar(arg1, arg2));
+		System.out.println("Producto de los argumentos:        (" + arg1 + " x " + arg2 + ") = " + multiplicar(arg1, arg2));
+		System.out.println("Cociente entero de los argumentos: (" + arg1 + " / " + arg2 + ") = " + dividir(arg1, arg2));
+
+		// Sale
+		System.exit(0);
+	}
+
+	/**
+	 * Procedimiento que lee un número entero de la consola y lo retorna
+	 * @return el número entero ingresado por el usuario
+	 */
+	public static int leerEnteroDeConsola() {
+		// Configura el scanner de la línea de comandos
+		final Scanner scanner = new Scanner(System.in);
+
+		// Lee lo ingresado por el usuario
+		final String tmp = scanner.next();
+
+		// Valida si es un entero y lo retorna
+		try {
+			return Integer.parseInt(tmp);
+		} catch (NumberFormatException nfe) {
+			System.err.println("Se esperaba un numero entero y se recibio '" + tmp + "'");
+			System.exit(0);
+			return 0;
+		}
 	}
 
 	/**
